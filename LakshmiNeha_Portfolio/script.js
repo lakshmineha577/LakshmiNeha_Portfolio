304cd90b75894a696731433fba9a290614e86950
Neha <81248720+lakshmineha577@users.noreply.github.com>
2025-05-26 12:10:57 +0530

    Delete LakshmiNeha_Portfolio directory

--- LakshmiNeha_Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Basic interaction (you can expand later)
-console.log("Welcome to Neha's Portfolio");
-// You can enhance with animations or navbar toggle here
-console.log("Portfolio Loaded Successfully");
-function toggleMenu() {
-  const nav = document.getElementById("navLinks");
-  nav.classList.toggle("show");
-}
