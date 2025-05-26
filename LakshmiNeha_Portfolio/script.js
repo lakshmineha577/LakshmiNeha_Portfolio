d3108ef5cedc8590f7774b94201e66120e20afa7
Neha <81248720+lakshmineha577@users.noreply.github.com>
2025-05-26 11:39:29 +0530

    Add files via upload

---
+++ LakshmiNeha_Portfolio/script.js
@@ -0,0 +1,8 @@
+// Basic interaction (you can expand later)
+console.log("Welcome to Neha's Portfolio");
+// You can enhance with animations or navbar toggle here
+console.log("Portfolio Loaded Successfully");
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("show");
+}
